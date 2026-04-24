521dc1f8fc328c19b4605178c2d854ed73a0ad87
ypratiksha912-cpu <260736683+ypratiksha912-cpu@users.noreply.github.com>
2026-04-24 18:07:00 +0530

    Update init.ts

--- src/firebase/init.ts
+++ src/firebase/init.ts
@@ -15,7 +15,7 @@ export function initializeFirebase() {
     let firebaseApp;
     try {
       // Attempt to initialize via Firebase App Hosting environment variables
-      firebaseApp = initializeApp();
+      firebaseApp = initializeApp(firebaseConfig);
     } catch (e) {
       // Only warn in production because it's normal to use the firebaseConfig to initialize
       // during development
